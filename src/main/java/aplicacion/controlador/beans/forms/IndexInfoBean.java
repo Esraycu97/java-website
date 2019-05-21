8d6df2302999ceed4b93c767047e8de101d5a97f
AlexMamani <alexander_mamani_22@hotmail.com>
2019-05-20 23:40:01 -0300

    Se Actualiza el nombre

---
+++ src/main/java/aplicacion/controlador/beans/forms/IndexInfoBean.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aplicacion.controlador.beans.forms;
+
+import javax.inject.Named;
+import javax.faces.view.ViewScoped;
+
+/**
+ *
+ * @author alexa
+ */
+@Named(value = "indexInfoBean")
+@ViewScoped
+public class IndexInfoBean {
+
+    /**
+     * Creates a new instance of IndexInfoBean
+     */
+    public IndexInfoBean() {
+    }
+    
+}
